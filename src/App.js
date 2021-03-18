08c0da3556e7d32ee1d103677f52fb0c7f0466bd
MatheusF99 <mc79846@gmail.com>
2021-03-17 21:13:53 -0300

    iniciando projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+
+  const [horizontal, setHorizontal] = useState(0)
+  const [vertical, setVertical] = useState(0)
+
+  useEffect(() => {
+    document.title = `clique (${horizontal})`
+  }, [horizontal])
+
+  
+  function handleIncrementHorizontal(){
+    setHorizontal(horizontal + 10 )
+  }
+
+  function handleDencrementHorizontal(){
+    setHorizontal(horizontal - 10 )
+  }
+
+  function handleIncrementVertical(){
+    setVertical(vertical + 10 )
+  }
+
+  function handleDencrementVertical(){
+    setVertical(vertical - 10 )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ top: horizontal, left: vertical }}>
+      <button onClick={handleDencrementHorizontal} >-</button>
+      <div className="vertical">
+        <button onClick={handleDencrementVertical} >-</button>
+          <p> ({horizontal})({vertical }) </p>
+        <button onClick={handleIncrementVertical} >+</button>        
+      </div>
+      <button onClick={handleIncrementHorizontal} >+</button>
     </div>
   );
 }
